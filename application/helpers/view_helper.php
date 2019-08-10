45a83a10526be762e1a2a64da5ca3b1169abd3a0
Firda Sepdiana <firdasepdiana@gmail.com>
2019-08-10 12:30:32 +0700

    Added view helper

---
+++ application/helpers/view_helper.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( ! function_exists('view')) {
+    function view($view, $data = []) {
+        $path = APPPATH . 'views';
+        $blade = new \Jenssegers\Blade\Blade($path, APPPATH . 'cache/view');
+
+        echo $blade->make($view, $data)->render();
+    }
+}
