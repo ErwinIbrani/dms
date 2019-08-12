32ffd1b941b0f526b2f5ec264389d8206e7854fc
Firda Sepdiana <firdasepdiana@gmail.com>
2019-08-12 15:24:06 +0700

    initial app layout

--- application/views/layouts/app.blade.php
+++ application/views/layouts/app.blade.php
@@ -6,11 +6,55 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IBS - @yield('page_title')</title>
+
+    <!-- BEGIN PLUGINS STYLES -->
+    <link rel="stylesheet" href="{{ base_url('assets/vendor/open-iconic/css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ base_url('assets/vendor/fontawesome/css/all.css') }}">
+    <link rel="stylesheet" href="{{ base_url('assets/vendor/tributejs/tribute.css') }}">
+    <!-- END PLUGINS STYLES -->
+
+    <!-- BEGIN THEME STYLES -->
+    <link rel="stylesheet" href="{{ base_url('assets/css/theme.min.css') }}" data-skin="default">
+    <link rel="stylesheet" href="{{ base_url('assets/css/theme-dark.min.css')  }}" data-skin="dark">
+    <link rel="stylesheet" href="{{ base_url('assets/css/custom.css') }}"><!-- Disable unused skin immediately -->
+
+    <script>
+        var skin = localStorage.getItem('skin') || 'default';
+        var unusedLink = document.querySelector('link[data-skin]:not([data-skin="' + skin + '"])');
+        unusedLink.setAttribute('rel', '');
+        unusedLink.setAttribute('disabled', true);
+    </script><!-- END THEME STYLES -->
+
 </head>
 <body>
 
-<h1>App Layout</h1>
-@yield('content')
+<div class="app">
+    @include('layouts.components.header')
+    @include('layouts.components.sidebar')
 
+    <main class="app-main">
+        <!-- .wrapper -->
+        <div class="wrapper">
+            @yield('content')
+        </div><!-- /.wrapper -->
+    </main>
+</div>
+
+
+<script src="{{ base_url('assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- END BASE JS -->
+
+<!-- BEGIN PLUGINS JS -->
+<script src="{{ base_url('assets/vendor/pace/pace.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/stacked-menu/stacked-menu.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/tributejs/tribute.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/chart.js/Chart.min.js') }}"></script>
+@stack('js')
+
+<!-- BEGIN THEME JS -->
+<script src="{{ base_url('assets/js/theme.min.js') }}"></script> <!-- END THEME JS -->
 </body>
 </html>
