d0b1f19ec22bc671ffc606c068af52b9995cc840
Firda Sepdiana <firdasepdiana@gmail.com>
2019-08-12 15:23:47 +0700

    update authentication layout

--- application/views/layouts/authentication.blade.php
+++ application/views/layouts/authentication.blade.php
@@ -6,9 +6,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IBS - @yield('page_title')</title>
+
+    <!-- BEGIN THEME STYLES -->
+    <link rel="stylesheet" href="{{ base_url('assets/css/theme.min.css') }}" data-skin="default">
+    <link rel="stylesheet" href="{{ base_url('assets/css/theme-dark.min.css')  }}" data-skin="dark">
+    <link rel="stylesheet" href="{{ base_url('assets/css/custom.css') }}"><!-- Disable unused skin immediately -->
+
+    <script>
+        var skin = localStorage.getItem('skin') || 'default';
+        var unusedLink = document.querySelector('link[data-skin]:not([data-skin="' + skin + '"])');
+        unusedLink.setAttribute('rel', '');
+        unusedLink.setAttribute('disabled', true);
+    </script><!-- END THEME STYLES -->
+
 </head>
-<body>
+<body class="">
 
 @yield('content')
+
+<script src="{{ base_url('assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ base_url('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- END BASE JS -->
+
+<!-- BEGIN THEME JS -->
+<script src="{{ base_url('assets/js/theme.min.js') }}"></script> <!-- END THEME JS -->
+
 </body>
 </html>
