22f024e87a5f8aff6bdf13431d5daf38628c6531
Erwin GTI <erwin@gti.co.id>
2019-09-03 14:18:30 +0700

    'addPagination'

--- application/views/admin/group-vendor/index.blade.php
+++ application/views/admin/group-vendor/index.blade.php
@@ -67,26 +67,6 @@
                 <div class="dataTables_paginate paging_simple_numbers" id="myTable_paginate">
                 <?= $pagination; ?>
                 </div>
-
-                <div class="dataTables_paginate paging_simple_numbers" id="myTable_paginate">
-                    <ul class="pagination justify-content-center">
-                        <li class="paginate_button page-item previous disabled" id="myTable_previous">
-                            <a href="#" aria-controls="myTable" data-dt-idx="0" tabindex="0" class="page-link">
-                                <i class="fa fa-lg fa-angle-left"></i>
-                            </a>
-                        </li>
-                        <li class="paginate_button page-item active">
-                            <a href="#" aria-controls="myTable" data-dt-idx="1" tabindex="0" class="page-link">1</a>
-                        </li>
-                        <li class="paginate_button page-item "><a href="#" aria-controls="myTable" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-                        <li class="paginate_button page-item "><a href="#" aria-controls="myTable" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
-                        <li class="paginate_button page-item "><a href="#" aria-controls="myTable" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="myTable" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item disabled" id="myTable_ellipsis"><a href="#" aria-controls="myTable" data-dt-idx="6" tabindex="0" class="page-link">…</a></li><li class="paginate_button page-item "><a href="#" aria-controls="myTable" data-dt-idx="7" tabindex="0" class="page-link">100</a></li><li class="paginate_button page-item next" id="myTable_next"><a href="#" aria-controls="myTable" data-dt-idx="8" tabindex="0" class="page-link">
-                                <i class="fa fa-lg fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
             </div>
         </div>
     </div>
