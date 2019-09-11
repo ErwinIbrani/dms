a05e2dd0a9d87c3048b6d8ea2ef7198d5c26911f
Firda Sepdiana <firdasepdiana@gmail.com>
2019-09-11 11:00:07 +0700

    Prototyping sidebar menu

--- application/views/layouts/components/sidebar.blade.php
+++ application/views/layouts/components/sidebar.blade.php
@@ -36,205 +36,56 @@
                             </li>
                         </ul><!-- /child menu -->
                     </li><!-- /.menu-item -->
-                    <li class="menu-header">Main</li><!-- /.menu-header -->
+
+					<li class="menu-item has-child">
+						<a href="#" class="menu-link">
+							<span class="menu-icon fas fa-globe"></span>
+							<span class="menu-text">Regional Management</span>
+						</a> <!-- child menu -->
+						<ul class="menu">
+							<li class="menu-item">
+								<a href="{{ site_url("/admin/province") }}" class="menu-link">Provinces</a>
+							</li>
+							<li class="menu-item">
+								<a href="{{ site_url("/admin/region") }}" class="menu-link">Regional</a>
+							</li>
+							<li class="menu-item">
+								<a href="{{ site_url("/admin/city") }}" class="menu-link">Cities</a>
+							</li>
+						</ul><!-- /child menu -->
+					</li><!-- /.menu-item -->
+
+                    <li class="menu-header">Procurement</li><!-- /.menu-header -->
                     <!-- .menu-item -->
                     <li class="menu-item">
-                        <a href="index.html" class="menu-link"><span class="menu-icon fas fa-home"></span> <span class="menu-text">Dashboard</span></a>
+                        <a href="{{ site_url('procurement/project') }}" class="menu-link"><span class="menu-icon fas fa-home"></span> <span class="menu-text">Project Assignment</span></a>
                     </li><!-- /.menu-item -->
                     <!-- .menu-item -->
-                    <li class="menu-item has-child">
-                        <a href="#" class="menu-link"><span class="menu-icon far fa-file"></span> <span class="menu-text">App Pages</span> <span class="badge badge-warning">New</span></a> <!-- child menu -->
-                        <ul class="menu"><li class="menu-subhead">App Pages</li>
-                            <li class="menu-item">
-                                <a href="page-clients.html" class="menu-link">Clients</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="page-teams.html" class="menu-link">Teams</a>
-                            </li>
-                            <li class="menu-item has-child">
-                                <a href="#" class="menu-link">Team</a> <!-- grand child menu -->
-                                <ul class="menu">
-                                    <li class="menu-item">
-                                        <a href="page-team.html" class="menu-link" tabindex="-1">Overview</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-team-feeds.html" class="menu-link" tabindex="-1">Feeds</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-team-projects.html" class="menu-link" tabindex="-1">Projects</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-team-members.html" class="menu-link" tabindex="-1">Members</a>
-                                    </li>
-                                </ul><!-- /grand child menu -->
-                            </li>
-                            <li class="menu-item has-active has-child">
-                                <a href="#" class="menu-link">Project</a> <!-- grand child menu -->
-                                <ul class="menu">
-                                    <li class="menu-item has-active">
-                                        <a href="page-project.html" class="menu-link">Overview</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-project-board.html" class="menu-link">Board</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-project-gantt.html" class="menu-link">Gantt View</a>
-                                    </li>
-                                </ul><!-- /grand child menu -->
-                            </li>
-                            <li class="menu-item">
-                                <a href="page-calendar.html" class="menu-link">Calendar</a>
-                            </li>
-                            <li class="menu-item has-child">
-                                <a href="#" class="menu-link">Invoices</a> <!-- grand child menu -->
-                                <ul class="menu">
-                                    <li class="menu-item">
-                                        <a href="page-invoices.html" class="menu-link" tabindex="-1">List</a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a href="page-invoice.html" class="menu-link" tabindex="-1">Details</a>
-                                    </li>
-                                </ul><!-- /grand child menu -->
-                            </li>
-                            <li class="menu-item">
-                                <a href="page-messages.html" class="menu-link">Messages</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="page-conversations.html" class="menu-link">Conversations</a>
-                            </li>
-                        </ul><!-- /child menu -->
-                    </li><!-- /.menu-item -->
-                    <!-- .menu-item -->
-                    <li class="menu-item has-child">
-                        <a href="#" class="menu-link"><span class="menu-icon oi oi-wrench"></span> <span class="menu-text">Auth</span></a> <!-- child menu -->
-                        <ul class="menu"><li class="menu-subhead">Auth</li>
-                            <li class="menu-item">
-                                <a href="auth-comingsoon-v1.html" class="menu-link" tabindex="-1">Coming Soon v1</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-comingsoon-v2.html" class="menu-link" tabindex="-1">Coming Soon v2</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-cookie-consent.html" class="menu-link" tabindex="-1">Cookie Consent</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-empty-state.html" class="menu-link" tabindex="-1">Empty State</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-error-v1.html" class="menu-link" tabindex="-1">Error Page v1</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-error-v2.html" class="menu-link" tabindex="-1">Error Page v2</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-error-v3.html" class="menu-link" tabindex="-1">Error Page v3</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-maintenance.html" class="menu-link" tabindex="-1">Maintenance</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-page-message.html" class="menu-link" tabindex="-1">Page Message</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-session-timeout.html" class="menu-link" tabindex="-1">Session Timeout</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-signin-v1.html" class="menu-link" tabindex="-1">Sign In v1</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-signin-v2.html" class="menu-link" tabindex="-1">Sign In v2</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-signup.html" class="menu-link" tabindex="-1">Sign Up</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-recovery-username.html" class="menu-link" tabindex="-1">Recovery Username</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-recovery-password.html" class="menu-link" tabindex="-1">Recovery Password</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="auth-lockscreen.html" class="menu-link" tabindex="-1">Screen Locked</a>
-                            </li>
-                        </ul><!-- /child menu -->
-                    </li><!-- /.menu-item -->
-                    <!-- .menu-item -->
-                    <li class="menu-item has-child">
-                        <a href="#" class="menu-link"><span class="menu-icon oi oi-person"></span> <span class="menu-text">User</span></a> <!-- child menu -->
-                        <ul class="menu"><li class="menu-subhead">User</li>
-                            <li class="menu-item">
-                                <a href="user-profile.html" class="menu-link" tabindex="-1">Profile</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-activities.html" class="menu-link" tabindex="-1">Activities</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-teams.html" class="menu-link" tabindex="-1">Teams</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-projects.html" class="menu-link" tabindex="-1">Projects</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-tasks.html" class="menu-link" tabindex="-1">Tasks</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-profile-settings.html" class="menu-link" tabindex="-1">Profile Settings</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-account-settings.html" class="menu-link" tabindex="-1">Account Settings</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-billing-settings.html" class="menu-link" tabindex="-1">Billing Settings</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="user-notification-settings.html" class="menu-link" tabindex="-1">Notification Settings</a>
-                            </li>
-                        </ul><!-- /child menu -->
-                    </li><!-- /.menu-item -->
-                    <!-- .menu-item -->
-                    <li class="menu-item has-child">
-                        <a href="#" class="menu-link"><span class="menu-icon oi oi-browser"></span> <span class="menu-text">Layouts</span> <span class="badge badge-subtle badge-success">+4</span></a> <!-- child menu -->
-                        <ul class="menu"><li class="menu-subhead">Layouts</li>
-                            <li class="menu-item">
-                                <a href="layout-blank.html" class="menu-link" tabindex="-1">Blank Page</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-nosearch.html" class="menu-link" tabindex="-1">Header no Search</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-horizontal-menu.html" class="menu-link" tabindex="-1">Horizontal Menu</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-fullwidth.html" class="menu-link" tabindex="-1">Full Width</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagenavs.html" class="menu-link" tabindex="-1">Page Navs</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagecover.html" class="menu-link" tabindex="-1">Page Cover</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagecover-img.html" class="menu-link" tabindex="-1">Cover Image</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagesidebar.html" class="menu-link" tabindex="-1">Page Sidebar</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagesidebar-fluid.html" class="menu-link" tabindex="-1">Sidebar Fluid</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-pagesidebar-hidden.html" class="menu-link" tabindex="-1">Sidebar Hidden</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="layout-custom.html" class="menu-link" tabindex="-1">Custom</a>
-                            </li>
-                        </ul><!-- /child menu -->
-                    </li><!-- /.menu-item -->
-                    <!-- .menu-item -->
-                    <li class="menu-item">
-                        <a href="landing-page.html" class="menu-link"><span class="menu-icon oi oi-dial"></span> <span class="menu-text">Landing Page</span></a>
-                    </li><!-- /.menu-item -->
-                    <!-- .menu-header -->
+					<li class="menu-header">Project</li><!-- /.menu-header -->
+					<!-- .menu-item -->
+					<li class="menu-item">
+						<a href="{{ site_url('project/pic') }}" class="menu-link"><span class="menu-icon fas fa-user-check"></span> <span class="menu-text">PIC Project</span></a>
+					</li><!-- /.menu-item -->
+					<li class="menu-header">Vendor</li><!-- /.menu-header -->
+
+					<li class="menu-item has-child">
+						<a href="#" class="menu-link">
+							<span class="menu-icon fas fa-file-invoice"></span>
+							<span class="menu-text">Project</span>
+						</a> <!-- child menu -->
+						<ul class="menu">
+							<li class="menu-item">
+								<a href="{{ site_url("admin/user-management/internal") }}" class="menu-link">Project</a>
+							</li>
+							<li class="menu-item">
+								<a href="{{ site_url("admin/user-management/internal") }}" class="menu-link">Running</a>
+							</li>
+							<li class="menu-item">
+								<a href="{{ site_url("admin/user/groups") }}" class="menu-link">Done</a>
+							</li>
+						</ul><!-- /child menu -->
+					</li><!-- /.menu-item -->
+
 
                 </ul><!-- /.menu -->
             </nav><!-- /.stacked-menu -->
