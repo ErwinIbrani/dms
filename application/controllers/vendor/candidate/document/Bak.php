0bfee4edfcd51eb774d4c5491cb34448158d3bd0
Firda Sepdiana <firdasepdiana@gmail.com>
2019-10-18 14:47:17 +0700

    added public bak layout

--- application/controllers/vendor/candidate/document/Bak.php
+++ application/controllers/vendor/candidate/document/Bak.php
@@ -46,9 +46,9 @@ class Bak extends CI_Controller
 		$document_id = $this->CandidateDocument_Model->save($candidate_document);
 
 		$api_endpoint = "https://selectpdf.com/api2/convert/";
-		$key = 'd4ca505b-0ca6-4f33-a075-afce3e313e82';
-		$layout_url = site_url('/vendor/candidate/document/bak/layout/' . $document_id);
-		$parameters = array('key' => $key, 'url' => $layout_url, 'web_page_width' => '816', 'page_numbers' => 'False');
+
+		$layout_url = site_url('/public/layout/bak/'. $document_id);
+		$parameters = array('key' => $this->config->item('pdf_key'), 'url' => $layout_url, 'web_page_width' => '816', 'page_numbers' => 'False');
 		$result = @file_get_contents("$api_endpoint?" . http_build_query($parameters));
 		if (!$result) {
 			echo "HTTP Response: " . $http_response_header[0] . "<br/>";
@@ -57,9 +57,10 @@ class Bak extends CI_Controller
 			echo "Error Message: " . $error['message'];
 		} else {
 			// set HTTP response headers
-			$local_file = './uploads/bak/BAK-'.time().'.pdf';
+			$filename = 'BAK-'.time().'.pdf';
+			$local_file = './uploads/bak/'.$filename;
 			file_put_contents($local_file, $result);
-			$this->CandidateDocument_Model->update($document_id, array('path' => $local_file));
+			$this->CandidateDocument_Model->update($document_id, array('path' => $filename));
 			return redirect('/vendor/candidate/document/bak/preview/' . $document_id);
 		}
 	}
@@ -73,112 +74,14 @@ class Bak extends CI_Controller
 		$candidate = $attribute->candidate[0];
 		$project = $this->Project_Model->findOne($candidate->project_id)->row();
 		$candidate_bak = $attribute->bak;
-
+		$path_document = '/uploads/bak/' . $document_candidate->path;
 		return view('vendor.candidate.document.preview', array(
 			'candidate' => $attribute->candidate[0],
-			'document' => $document_candidate
+			'document' => $document_candidate,
+			'path_document' => $path_document
 		));
 
 	}
 
-	public function layout($document_id)
-	{
-		$document_candidate = $this->CandidateDocument_Model->findOne($document_id)->row();
-		$attribute = json_decode($document_candidate->attribute);
 
-		$candidate = $attribute->candidate[0];
-		$project = $this->Project_Model->findOne($candidate->project_id)->row();
-		$candidate_bak = $attribute->bak;
-
-		switch (date('N', strtotime($candidate_bak->negotiation_at))) {
-			case '1':
-				$day = 'Senin';
-				break;
-			case '2':
-				$day = 'Selasa';
-				break;
-			case '3':
-				$day = 'Rabu';
-				break;
-			case '4':
-				$day = 'Kamis';
-				break;
-			case '5':
-				$day = 'Jum\'at';
-				break;
-			case '6':
-				$day = 'Sabtu';
-				break;
-			case '7':
-				$day = 'Minggu';
-				break;
-		}
-
-		if (property_exists($candidate_bak, 'pph')) {
-			$pph = true;
-		} else {
-			$pph = false;
-		}
-
-		if (property_exists($candidate_bak, 'non_pkp')) {
-			$non_pkp = true;
-		} else {
-			$non_pkp = false;
-		}
-
-		if (property_exists($candidate_bak, 'pkp')) {
-			$pkp = true;
-		} else {
-			$pkp = false;
-		}
-
-
-		return view('vendor.candidate.document.bak.layout', array(
-			'negotiation_by' => $candidate_bak->negotiation_by,
-			'day' => $day,
-			'date' => date('d', strtotime($candidate_bak->negotiation_at)),
-			'month' => date('m', strtotime($candidate_bak->negotiation_at)),
-			'year' => date('Y', strtotime($candidate_bak->negotiation_at)),
-			'first_person' => $candidate_bak->first_person,
-			'second_person' => $candidate_bak->second_person,
-			'site_id' => $project->site_id_ibs,
-			'site_name' => $project->site_name,
-			'owner_name' => ucwords($candidate->owner_name),
-			'owner_type' => $candidate_bak->owner_type,
-			'owner_phone_number' => $candidate->phone_number,
-			'owner_address' => $candidate->owner_address,
-			'authorized_name' => (!empty($candidate_bak->authorized_name)) ? $candidate_bak->authorized_name : '-',
-			'authorized_id_card' => (!empty($candidate_bak->authorized_id_card)) ? $candidate_bak->authorized_id_card : '-',
-			'authorized_phone' => (!empty($candidate_bak->authorized_phone)) ? $candidate_bak->authorized_phone : '-',
-			'authorized_address' => (!empty($candidate_bak->authorized_address)) ? $candidate_bak->authorized_address : '-',
-			'land_type' => ucwords($candidate_bak->land_type),
-			'total_price_rent' => number_format($candidate_bak->total_price_rent, 0, ',', '.'),
-			'be_regarded' => $candidate_bak->be_regarded,
-			'rental_period' => $candidate_bak->rental_period,
-			'payment_type' => ucwords($candidate_bak->payment_type),
-			'bank_name' => $candidate_bak->bank_name,
-			'account_number' => $candidate_bak->account_number,
-			'account_name' => $candidate_bak->account_name,
-			'phase_1_percent' => $candidate_bak->phase_1_percent,
-			'phase_1_nominal' => number_format($candidate_bak->phase_1_nominal, 0, ',', '.'),
-			'pahase_2_percent' => $candidate_bak->phase_2_percent,
-			'phase_2_nominal' => number_format($candidate_bak->phase_2_nominal, 0, ',', '.'),
-			'note' => (!empty($candidate_bak->note)) ? $candidate_bak->note : '-',
-			'bumi' => $candidate_bak->bumi,
-			'bangunan' => $candidate_bak->bangunan,
-			'space_dimension' => 12,
-			'access_road' => 12,
-			'non_pkp' => $non_pkp,
-			'pkp' => $pkp,
-			'pph' => $pph,
-			'rent_object_status' => $candidate_bak->rent_object_status,
-			'status_hak_object' => $candidate_bak->status_hak_object,
-			'status_hak_object_nomor' => $candidate_bak->status_hak_object_nomor,
-			'status_hak_object_lain' => $candidate_bak->status_hak_object_lain,
-			'user_ibs_name' => $candidate_bak->user_ibs_name,
-			'user_ibs_position' => $candidate_bak->user_ibs_position,
-			'saksi_1_name' => $candidate_bak->saksi_1_name,
-			'saksi_2_name' => $candidate_bak->saksi_2_name,
-		));
-	}
 }
