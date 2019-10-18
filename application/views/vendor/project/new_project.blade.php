c5e95becb01884f8f8cc29a9188835d5b5a561a3
Firda Sepdiana <firdasepdiana@gmail.com>
2019-10-18 11:24:57 +0700

    Update new_project.blade.php

--- application/views/vendor/project/new_project.blade.php
+++ application/views/vendor/project/new_project.blade.php
@@ -71,7 +71,7 @@
 
 											<td class="text-right">
 												<div class="list-group-item-figure">
-													@if($project->status === 'New' && $project->assignment_type === 'SITAC')
+													@if($project->status === 'new' && $project->assignment_type === 'SITAC')
 														<a href="{{ site_url('/vendor/candidates/create/' . $project->id . '/' . $vendor_id) }}" class="btn btn-sm btn-icon btn-secondary stop-propagation"><i class="fas fa-ellipsis-h"></i></a>
 													@endif
 												</div>
