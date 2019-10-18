ea984752a558dca543451aa9e5f3d3225f709067
IBS <firda@gti.co.id>
2019-10-18 08:47:13 +0000

    merge config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://dms.ibstower.test/';   //https://dms.ibstower.test/
+$config['base_url'] = 'https://dms2.gti.co.id/';   //https://dms.ibstower.test/
 
 
 /*
