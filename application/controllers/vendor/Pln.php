d53327642bae7ed05132338d11cf9c77cac590c1
Firda Sepdiana <firdasepdiana@gmail.com>
2019-10-31 11:34:39 +0700

    pln

---
+++ application/controllers/vendor/Pln.php
@@ -0,0 +1,29 @@
+<?php
+
+class Pln extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array('Project_Model', 'Document_Model'));
+	}
+
+	public function index($project_id)
+	{
+		$project = $this->findProjectById($project_id);
+
+		return view('vendor.cme.pln.index', array('project' => $project));
+	}
+
+	/**
+	 * Find project by ID of the project.
+	 *
+	 * @param $project_id
+	 * @return mixed
+	 */
+	private function findProjectById($project_id)
+	{
+		$project = $this->Project_Model->findOne($project_id);
+		return $project->row();
+	}
+}
